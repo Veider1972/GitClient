ecff56fd93a28576e909cccb0f1be44ae9bc3224
Корховой Вадим <veider@inbox.ru>
2022-06-11 19:46:48 +0300

    Урок 02. Реализован паттерн MVP, добавлен просмотр страницы пользователя

---
+++ app/src/main/java/ru/veider/gitclient/ui/gituser/GitUserFragment.kt
@@ -0,0 +1,54 @@
+package ru.veider.gitclient.ui.gituser
+
+import android.graphics.Bitmap
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import ru.veider.gitclient.R
+import ru.veider.gitclient.databinding.FragmentUserBinding
+
+class GitUserFragment : Fragment(R.layout.fragment_user) {
+    private var isPageLoaded = false
+    companion object {
+        private lateinit var gotoUrl: String
+
+        fun newInstance(url: String): GitUserFragment {
+            gotoUrl = url
+            return GitUserFragment()
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        retainInstance = true
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val fragmentView = view
+        FragmentUserBinding.bind(view).webView.apply {
+            webViewClient = object : WebViewClient() {
+
+                override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                    if (!isPageLoaded) {
+                        FragmentUserBinding.bind(fragmentView).progress.visibility = View.VISIBLE
+                    }
+                    super.onPageStarted(view, url, favicon)
+                }
+
+                override fun onPageFinished(view: WebView?, url: String?) {
+                    FragmentUserBinding.bind(fragmentView).progress.visibility = View.GONE
+                    isPageLoaded = true
+                    super.onPageFinished(view, url)
+                }
+            }
+            if (gotoUrl.isNotEmpty()) {
+                loadUrl(gotoUrl)
+            }
+
+        }
+    }
+
+}
--- app/src/main/java/ru/veider/gitclient/ui/gitusers/GitUsersFragment.kt
+++ app/src/main/java/ru/veider/gitclient/ui/gitusers/GitUsersFragment.kt
@@ -1,65 +1,54 @@
 package ru.veider.gitclient.ui.gitusers
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import ru.veider.gitclient.R
 import ru.veider.gitclient.app
-import ru.veider.gitclient.databinding.FragmentMainBinding
+import ru.veider.gitclient.databinding.FragmentUsersBinding
 import ru.veider.gitclient.domain.entity.GitUsersEntity
-import ru.veider.gitclient.domain.repository.GitUsersRepository
+import ru.veider.gitclient.presenter.gitusers.GitUsersContract
+import ru.veider.gitclient.presenter.gitusers.GitUsersPresenter
+import ru.veider.gitclient.ui.TAG
 
-class GitUsersFragment : Fragment() {
+class GitUsersFragment : Fragment(), GitUsersContract.View {
 
     companion object {
         fun newInstance() = GitUsersFragment()
     }
 
-    private lateinit var binding : FragmentMainBinding
-    private val adapter = GitUsersAdapter()
-    private val repository: GitUsersRepository by lazy { app.gitUsersRepository }
+    private var _binding: FragmentUsersBinding? = null
+    private val binding get() = _binding!!
+    private val adapter by lazy { GitUsersAdapter(this) }
+    private lateinit var presenter: GitUsersContract.Presenter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        presenter = GitUsersPresenter(app.gitUsersRepository)
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        val view = inflater.inflate(R.layout.fragment_main, container, false)
-        binding = FragmentMainBinding.bind(view)
+        val view = inflater.inflate(R.layout.fragment_users, container, false)
+        _binding = FragmentUsersBinding.bind(view)
+        retainInstance = true
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        presenter.attach(this)
         initViews()
     }
 
     private fun initViews() {
-        loadData()
+        presenter.onRefresh()
         initRecyclerView()
-    }
-
-    private fun loadData() {
-        showProgress(true)
-        repository.getUsers(
-            onSuccess = {
-                showProgress(false)
-                onDataLoaded(it)
-            },
-            onError = {
-                showProgress(false)
-                onError(it)
-            }
-        )
-    }
-
-    private fun onDataLoaded(data: List<GitUsersEntity>) {
-        adapter.setData(data)
-    }
-
-    private fun onError(throwable: Throwable) {
-        Toast.makeText(requireContext(), throwable.message, Toast.LENGTH_SHORT).show()
+        //showProgress(false)
     }
 
     private fun initRecyclerView() {
@@ -67,8 +56,25 @@ class GitUsersFragment : Fragment() {
         binding.usersRecyclerView.adapter = adapter
     }
 
-    private fun showProgress(isVisible: Boolean) {
-        binding.progressBar.isVisible = isVisible
-        binding.usersRecyclerView.isVisible = !isVisible
+    override fun showUsers(users: List<GitUsersEntity>) {
+        adapter.setData(users)
+    }
+
+    override fun showError(throwable: Throwable) {
+        Toast.makeText(requireContext(), throwable.message, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun showProgress(showingProgress: Boolean) {
+        binding.apply {
+            progressBar.visibility = if (showingProgress) View.VISIBLE else View.GONE
+            usersRecyclerView.visibility = if (showingProgress) View.GONE else View.VISIBLE
+        }
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        presenter.detach()
+        _binding = null
     }
 }
